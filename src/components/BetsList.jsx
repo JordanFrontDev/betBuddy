aafae33eb053121b5c47314892a2a96b4861b1b5
Jordan Vinicius <jordan.frontdev@hotmail.com>
2024-03-21 10:28:18 -0300

    changes to BetsList component to ensure reusability

--- src/components/BetsList.jsx
+++ src/components/BetsList.jsx
@@ -1,7 +1,15 @@
 import styles from "./BetsList.module.css";
 import Bet from "./Bet";
 
-function BetsList({ bets, win = false }) {
+function BetsList({ bets = [], win = false }) {
+  // if the win prop is true then copy the bets array and sort alfabetically
+  // if not then just return an empty array
+  const winnersBets = win
+    ? [...bets].sort((a, b) => {
+        return a.name.localeCompare(b.name);
+      })
+    : [];
+
   return (
     <>
       {win ? (
@@ -11,9 +19,17 @@ function BetsList({ bets, win = false }) {
       )}
 
       {/* If there's at least 1 bet then render this */}
-      {bets.length > 0 && (
+      {bets.length > 0 && !win && (
         <ul className={styles.list}>
           {bets.map((bet) => (
+            <Bet bet={bet} key={bet.id} />
+          ))}
+        </ul>
+      )}
+
+      {winnersBets.length > 0 && (
+        <ul className={styles.list}>
+          {winnersBets.map((bet) => (
             <Bet bet={bet} key={bet.id} />
           ))}
         </ul>
